e1700fd4a5a66386e53c997098c6da1fc218682f
Maxim Ledaine <maxim.ledaine@outlook.com>
2019-08-08 13:42:00 +0200

    investment page

--- public/config.js
+++ public/config.js
@@ -2,10 +2,10 @@ export default({
   tftNetwork: 'testnet',
   gftNetwork: 'testnet',
   nbhFaucet: `https://faucet.testnet.nbh-digital.com/`,
-  nbhExplorer: `http://localhost:2015/explorer/`,
+  nbhExplorer: `https://explorer.testnet.nbh-digital.com`,
   appId: `${window.location.host}`,
-  botFrontEnd: 'http://localhost:8082',
-  botBackend: 'http://localhost:5000',
+  botFrontEnd: 'https://login.staging.jimber.org/',
+  botBackend: 'https://login.staging.jimber.org/',
   redirect_url: '/login',
   scope: 'user:keys'
 })
