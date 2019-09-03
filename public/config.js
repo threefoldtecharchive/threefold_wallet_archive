eb036967bd75c2c4876b62798698335af3297e69
tobiaschielens <tobias.chielens@gmail.com>
2019-09-03 15:53:26 +0200

    changed bot backend url

--- public/config.js
+++ public/config.js
@@ -1,8 +1,8 @@
 export default({
   tftNetwork: 'testnet',
   appId: `${window.location.host}`,
-  botFrontEnd: 'https://login.staging.jimber.org/',
-  botBackend: 'https://login.staging.jimber.org/',
+  botFrontEnd: 'http://192.168.2.120:8081/',
+  botBackend: 'http://192.168.2.120:5000',
   redirect_url: '/login',
-  scope: 'user:keys'
+  scope: JSON.stringify({doubleName: true, keys: true})
 })
