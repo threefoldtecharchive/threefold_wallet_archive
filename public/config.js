a7b68592b9dd180f46f705b942c7f4c6eadd0ad7
GillesMommerency <gilles.mommerency@student.howest.be>
2019-09-12 11:44:01 +0200

    Fixed merge with master

--- public/config.js
+++ public/config.js
@@ -1,12 +1,11 @@
 export default({
   tftNetwork: 'testnet',
   gftNetwork: 'testnet',
-  nbhFaucet: `https://faucet.testnet.nbh-digital.com/`,
-  nbhExplorer: `https://explorer2.testnet.nbh-digital.com/explorer`,
+  nbhFaucet: 'https://faucet.testnet.nbh-digital.com/',
+  nbhExplorer: 'https://explorer2.testnet.nbh-digital.com/explorer',
   appId: `${window.location.host}`,
-  botFrontEnd: 'http://192.168.2.60:8081/',
-  botBackend: 'http://192.168.2.60:5000',
+  botFrontEnd: 'https://login.staging.jimber.org/',
+  botBackend: 'https://login.staging.jimber.org',
   redirect_url: '/login',
-  scope: JSON.stringify({doubleName: true, derivedSeed: true} )
+  scope: JSON.stringify({doubleName: true, derivedSeed: true})
 })
-
