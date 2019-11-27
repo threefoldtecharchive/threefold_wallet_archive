e0d08f973f8eb6e8de46ebed1d6d1b58f0b0befe
Mathias <mathias.de.weerdt@gmail.com>
2019-11-27 13:59:47 +0100

    Changed color and updated versions

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,7 +5,7 @@ import 'vuetify/src/stylus/app.styl'
 Vue.use(Vuetify, {
   iconfont: 'md',
   theme: {
-    primary: '#34495e',
+    primary: '#2d4052',
     accent: '#1abc9c',
     gold: '#dea600',
     error: '#CE796B'
