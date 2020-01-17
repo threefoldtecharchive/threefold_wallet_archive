15a05f090eb15d28552dc9a7f71b066ce902ec58
Jonas Wijne <jonas.wijne@jimber.org>
2020-01-17 16:21:48 +0100

    Rollback unnecessary config change

--- public/config.js
+++ public/config.js
@@ -1,8 +1,11 @@
 export default({
   tftNetwork: 'testnet',
+  gftNetwork: 'testnet',
+  nbhFaucet: 'https://faucet.testnet.nbh-digital.com/',
+  nbhExplorer: 'https://explorer2.testnet.nbh-digital.com/explorer',
   appId: `${window.location.host}`,
   botFrontEnd: 'https://login.staging.jimber.org/',
-  botBackend: 'https://login.staging.jimber.org/',
+  botBackend: 'https://login.staging.jimber.org',
   redirect_url: '/login',
-  scope: 'user:keys'
+  scope: JSON.stringify({ doubleName: true, derivedSeed: true })
 })
