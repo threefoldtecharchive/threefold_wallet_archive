e0cad43a08a6a0c6d69273208111b50a02e84078
Jonas Wijne <jonas.wijne@jimber.org>
2020-01-23 11:30:14 +0100

    Refactor authStore

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,38 +1,35 @@
 /* eslint-disable no-console */
-import botService from '../services/3botService';
-import config from '../../public/config';
-import cryptoService from '../services/cryptoService';
-import { decodeBase64 } from 'tweetnacl-util';
+import botService from '../services/3botService'
+import config from '../../public/config'
+import cryptoService from '../services/cryptoService'
+import { decodeBase64 } from 'tweetnacl-util'
 
-
-//@todo: does the authstore need to exist anymore
+// @TODO: does the authstore need to exist anymore
 
 export default {
   state: {
-    state: localStorage.getItem('state') || null,
-    keys: localStorage.getItem('tempKeys')
-      ? JSON.parse(localStorage.getItem('tempKeys'))
+    state: window.localStorage.getItem('state') || null,
+    keys: window.localStorage.getItem('tempKeys')
+      ? JSON.parse(window.localStorage.getItem('tempKeys'))
       : null,
     loginUrl: null
   },
   actions: {
     async generateLoginUrl (context) {
-      context.dispatch('clearStorage');
-      var state = '';
+      context.dispatch('clearStorage')
+      var state = ''
       var characters =
-        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-      var charactersLength = characters.length;
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+      var charactersLength = characters.length
       for (var i = 0; i < 20; i++) {
-        state += characters.charAt(
-          Math.floor(Math.random() * charactersLength)
-        );
+        state += characters.charAt(Math.floor(Math.random() * charactersLength))
       }
 
-      var keys = await cryptoService.generateKeys();
-      var appid = config.appId;
-      var scope = config.scope;
-      context.commit('setState', state);
-      context.commit('setKeys', keys);
+      var keys = await cryptoService.generateKeys()
+      var appid = config.appId
+      var scope = config.scope
+      context.commit('setState', state)
+      context.commit('setKeys', keys)
 
       context.commit(
         'setLoginUrl',
@@ -41,129 +38,122 @@ export default {
         }?state=${state}&scope=${scope}&appid=${appid}&publickey=${encodeURIComponent(
           keys.publicKey
         )}&redirecturl=${encodeURIComponent(config.redirect_url)}`
-      );
+      )
     },
-    async checkResponse(context, responseUrl) {
+    async checkResponse (context, responseUrl) {
       if (responseUrl.searchParams.get('error')) {
-        context.commit('setFatalError', responseUrl.searchParams.get('error'));
-      } else {
-        // this is the iphone path, should be removed. Uses derivedseed straight from URL after hash
-        var username = responseUrl.searchParams.get('username');
-        var signedHash = responseUrl.searchParams.get('signedhash');
-        var directLoginData;
-        if (window.location.hash) {
-          directLoginData = window.location.hash
-            .substr(1)
-            .split('&')
-            .reduce(function(result, item) {
-              var parts = item.split('=');
-              result[parts[0]] = decodeURIComponent(parts[1]);
-              return result;
-            }, {});
-          var newSeed = new Uint8Array(
-            decodeBase64(directLoginData.derivedSeed)
-          );
-          const userObject = {
-            doubleName: directLoginData.username,
-            seed: newSeed
-          };
-          await context.dispatch('login', userObject);
+        context.commit('setFatalError', responseUrl.searchParams.get('error'))
+        return
+      }
 
-          let importedWallets = JSON.parse(
-            localStorage.getItem('importedWallets')
-          );
-          if (importedWallets != null && importedWallets) {
-            for (let user of importedWallets.filter(
-              x => x.doubleName === directLoginData.doubleName
-            )) {
-              user.seed = new Uint8Array(user.seed);
-              await context.dispatch('importWallet', user);
+      // this is the iphone path, should be removed. Uses derivedseed straight from URL after hash
+      var username = responseUrl.searchParams.get('username')
+      var signedHash = responseUrl.searchParams.get('signedhash')
+      var directLoginData
+      if (window.location.hash) {
+        directLoginData = window.location.hash
+          .substr(1)
+          .split('&')
+          .reduce(function (result, item) {
+            var parts = item.split('=')
+            result[parts[0]] = decodeURIComponent(parts[1])
+            return result
+          }, {})
+        var newSeed = new Uint8Array(decodeBase64(directLoginData.derivedSeed))
+        const userObject = {
+          doubleName: directLoginData.username,
+          seed: newSeed
+        }
+        await context.dispatch('login', userObject)
+
+        const importedWallets = JSON.parse(
+          window.localStorage.getItem('importedWallets')
+        )
+        if (!(importedWallets != null && importedWallets)) {
+          return
+        }
+        for (const user of importedWallets.filter(
+          x => x.doubleName === directLoginData.doubleName
+        )) {
+          user.seed = new Uint8Array(user.seed)
+          await context.dispatch('importWallet', user)
+        }
+        return
+      }
+      // this is the Android and normal login path
+      try {
+        const response = await botService.getUserData(username)
+
+        if (
+          signedHash &&
+          context.getters.state !==
+            (await cryptoService.validateSignature(
+              signedHash,
+              response.data.publicKey
+            ))
+        ) {
+          context.commit('setFatalError', 'Invalid state.')
+        }
+        // http://localhost:8080/login#username=ivan&derivedSeed=abc123
+        const responseData = responseUrl.searchParams.get('data')
+        if (!responseData) {
+          context.commit('setFatalError', 'Got no data from 3bot')
+          return
+        }
+
+        // eslint-disable-next-line no-unused-vars
+        const data = JSON.parse(responseData)
+        const keys = context.getters.keys
+        const userData = {}
+        try {
+          const decryptedData = await cryptoService.decrypt(
+            data.ciphertext,
+            data.nonce,
+            keys.privateKey,
+            response.data.publicKey
+          )
+          if (decryptedData) {
+            const decrypted = JSON.parse(decryptedData)
+            for (const k in decrypted) {
+              if (decrypted.hasOwnProperty(k)) {
+                userData[k] = decrypted[k]
+              }
             }
           }
-        } else {
-          // this is the Android and normal login path
-          botService
-            .getUserData(username)
-            .then(async response => {
-              if (
-                signedHash &&
-                context.getters.state !==
-                  (await cryptoService.validateSignature(
-                    signedHash,
-                    response.data.publicKey
-                  ))
-              ) {
-                context.commit('setFatalError', `Invalid state.`);
-              }
-              // http://localhost:8080/login#username=ivan&derivedSeed=abc123
-              var data = responseUrl.searchParams.get('data');
-              if (data) {
-                data = JSON.parse(data);
-                var keys = context.getters.keys;
-                var userData = {};
-                await cryptoService
-                  .decrypt(
-                    data.ciphertext,
-                    data.nonce,
-                    keys.privateKey,
-                    response.data.publicKey
-                  )
-                  .then( async decrypted => {
-                    if (decrypted) {
-                      decrypted = JSON.parse(decrypted);
-                      for (var k in decrypted) {
-                        if (decrypted.hasOwnProperty(k)) {
-                          userData[k] = decrypted[k];
-                        }
-                      }
-                    }
-                    var newSeed = new Uint8Array(
-                      decodeBase64(userData.derivedSeed)
-                    );
-                    const userObject = {
-                      doubleName: username,
-                      seed: newSeed
-                    };
 
-                    await context.dispatch('login', userObject);
-                  })
-                  .catch(e => {
-                    context.commit('setImportingWallets', false)
-                    console.log(e)
-                      return context.commit(
-                        'setFatalError',
-                        e.message
-                      )
-                    }
-                  );
-              } else {
-                context.commit('setFatalError', 'Got no data from 3bot');
-              }
-            })
-            .catch(e => {
-              // context.commit('setFatalError', 'Signature failed, please try again.')
-              // We can't do this because we need to be able to login from the browser. (I think)
-              context.dispatch('generateLoginUrl');
-            });
+          newSeed = new Uint8Array(decodeBase64(userData.derivedSeed))
+          const userObject = {
+            doubleName: username,
+            seed: newSeed
+          }
+          await context.dispatch('login', userObject)
+        } catch (e) {
+          context.commit('setImportingWallets', false)
+          console.log(e)
+          return context.commit('setFatalError', e.message)
         }
+      } catch (e) {
+        // context.commit('setFatalError', 'Signature failed, please try again.')
+        // We can't do this because we need to be able to login from the browser. (I think)
+        context.dispatch('generateLoginUrl')
       }
     },
-    clearStorage(context) {
-      context.commit('setState', null);
-      context.commit('setKeys', null);
+    clearStorage (context) {
+      context.commit('setState', null)
+      context.commit('setKeys', null)
     }
   },
   mutations: {
-    setKeys(state, keys) {
-      localStorage.setItem('tempKeys', JSON.stringify(keys));
-      state.keys = keys;
+    setKeys (state, keys) {
+      window.localStorage.setItem('tempKeys', JSON.stringify(keys))
+      state.keys = keys
     },
-    setState(state, stateHash) {
-      localStorage.setItem('state', stateHash);
-      state.state = stateHash;
+    setState (state, stateHash) {
+      window.localStorage.setItem('state', stateHash)
+      state.state = stateHash
     },
-    setLoginUrl(state, url) {
-      state.loginUrl = url;
+    setLoginUrl (state, url) {
+      state.loginUrl = url
     }
   },
   getters: {
@@ -171,4 +161,4 @@ export default {
     state: state => state.state,
     loginUrl: state => state.loginUrl
   }
-};
+}
