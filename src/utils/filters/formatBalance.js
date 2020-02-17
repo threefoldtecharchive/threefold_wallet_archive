dcf8dc275d0e29bc8c796e83682b2d35f078f359
Jonas Wijne <jonas.wijne@jimber.org>
2020-02-17 06:56:12 +0100

    styling update

--- src/utils/filters/formatBalance.js
+++ src/utils/filters/formatBalance.js
@@ -1,7 +1,26 @@
-import { round } from '@jimber/stellar-crypto/dist/tfchain/org.transcrypt.__runtime__';
+const SI_SYMBOL = ['', 'k', 'M', 'G', 'T', 'P', 'E'];
 
 export const formatBalance = value => {
-  if (value) {
-    return round(value / 10000, 3).toFixed(3);
+  if (!value) {
+    return;
   }
+  const number = Math.round(value / 10) / 1000;
+
+  // what tier? (determines SI symbol)
+  const tier = (Math.log10(number) / 3) | 0;
+
+  // if zero, we don't need a suffix
+  if (tier == 0) {
+    return number.toFixed(3);
+  }
+
+  // get suffix and determine scale
+  const suffix = SI_SYMBOL[tier];
+  const scale = Math.pow(10, tier * 3);
+
+  // scale the number
+  const scaled = number / scale;
+
+  // format number and add suffix
+  return scaled.toFixed(3) + suffix;
 };
