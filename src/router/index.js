45a4bd2d3adf1edab8de9197e10918d5d1f3d9d0
Jasper Schreel <jasperschreel@gmail.com>
2020-02-24 13:28:18 +0100

    tranfer accountName fix

--- src/router/index.js
+++ src/router/index.js
@@ -10,150 +10,150 @@ import store from '../store';
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home,
-    meta: {
-      title: 'wallet',
-      transfer: 'transfer',
-      accent: 'accent',
-      tags: ['showTransferButton'],
+const routes = [{
+        path: '/',
+        name: 'home',
+        component: Home,
+        meta: {
+            title: 'wallet',
+            transfer: 'transfer',
+            accent: 'accent',
+            tags: ['showTransferButton'],
+        },
     },
-  },
-  {
-    path: '/init',
-    name: 'init',
-    meta: {
-      title: 'initialize',
-      accent: 'accent',
+    {
+        path: '/init',
+        name: 'init',
+        meta: {
+            title: 'initialize',
+            accent: 'accent',
+        },
+        component: Init,
     },
-    component: Init,
-  },
-  {
-    path: '/devview',
-    name: 'devview',
-    meta: {
-      title: 'devview',
-      accent: 'accent',
+    {
+        path: '/devview',
+        name: 'devview',
+        meta: {
+            title: 'devview',
+            accent: 'accent',
+        },
+        component: DevView,
     },
-    component: DevView,
-  },
-  {
-    path: '/addwallet',
-    name: 'addwallet',
-    meta: {
-      accent: 'accent',
-      title: 'import/create wallet',
+    {
+        path: '/addwallet',
+        name: 'addwallet',
+        meta: {
+            accent: 'accent',
+            title: 'import/create wallet',
+        },
+        component: createWallet,
     },
-    component: createWallet,
-  },
-  {
-    path: '/details/:account',
-    name: 'details',
-    meta: {
-      title: 'details',
-      accent: 'accent',
+    {
+        path: '/details/:account',
+        name: 'details',
+        meta: {
+            title: 'details',
+            transfer: 'transfer',
+            accent: 'accent',
+        },
+        component: Details,
     },
-    component: Details,
-  },
-  {
-    path: '/transfer',
-    name: 'transfer',
-    meta: {
-      accent: 'accent',
-      overview: 'home',
-      history: 'details',
+    {
+        path: '/transfer/:account',
+        name: 'transfer',
+        meta: {
+            accent: 'accent',
+            overview: 'home',
+            history: 'details',
+        },
+        component: Transfer,
     },
-    component: Transfer,
-  },
-  {
-    path: '/walletinfo/:account',
-    name: 'wallet info',
+    {
+        path: '/walletinfo/:account',
+        name: 'wallet info',
 
-    meta: {
-      accent: 'accent',
-      transfer: 'transfer',
-      info: {
-        title: 'wallet info',
-        text: 'this is info',
-      },
-    },
-    component: () =>
-      import(/* webpackChunkName: "wallet-info" */ '../views/WalletInfo'),
-  }, // {
-  //   path: '/profile',
-  //   name: 'profile',
-  //   meta: {
-  //     accent: 'accent'
-  //   },
-  //   component: () => import(/* webpackChunkName: "profile-page" */ './views/profile')
-  // },  {
-  //   path: '/settings',
-  //   name: 'settings',
-  //   meta: {
-  //     accent: 'accent'
-  //   },
-  //   component: () => import(/* webpackChunkName: "settings-page" */ './views/settings')
-  // }, {
-  //   path: '/transfer',
-  //   name: 'transfer',
-  //   meta: {
-  //     accent: 'accent',
-  //     overview: 'home',
-  //     history: 'details',
-  //     info: {
-  //       title: 'one hell of a title',
-  //       text: 'this is info'
-  //     }
-  //   },
-  //   component: () => import(/* webpackChunkName: "transfer-page" */ './views/transfer')
-  // }, {
-  //   path: '/login',
-  //   name: 'login',
-  //   meta: {
-  //     accent: 'accent'
-  //   },
-  //   component: () => import(/* webpackChunkName: "login-page" */ './views/login')
-  // }, {
-  //   path: '/Init',
-  //   name: 'Init',
-  //   meta: {
-  //     title: 'Initialize',
-  //     accent: 'accent'
-  //   },
-  //   component: () => import(/* webpackChunkName: "Init-page" */ './views/Init')
-  // }, {
-  //   path: '/error',
-  //   name: 'error',
-  //   meta: {
-  //     accent: 'accent'
-  //   },
-  //   component: () => import(/* webpackChunkName: "error-page" */ './views/errorpage')
-  // }, {
-  //   path: '*',
-  //   redirect: '404',
-  //   meta: {
-  //     accent: 'accent'
-  //   }
-  // }
+        meta: {
+            accent: 'accent',
+            transfer: 'transfer',
+            info: {
+                title: 'wallet info',
+                text: 'this is info',
+            },
+        },
+        component: () =>
+            import ( /* webpackChunkName: "wallet-info" */ '../views/WalletInfo'),
+    }, // {
+    //   path: '/profile',
+    //   name: 'profile',
+    //   meta: {
+    //     accent: 'accent'
+    //   },
+    //   component: () => import(/* webpackChunkName: "profile-page" */ './views/profile')
+    // },  {
+    //   path: '/settings',
+    //   name: 'settings',
+    //   meta: {
+    //     accent: 'accent'
+    //   },
+    //   component: () => import(/* webpackChunkName: "settings-page" */ './views/settings')
+    // }, {
+    //   path: '/transfer',
+    //   name: 'transfer',
+    //   meta: {
+    //     accent: 'accent',
+    //     overview: 'home',
+    //     history: 'details',
+    //     info: {
+    //       title: 'one hell of a title',
+    //       text: 'this is info'
+    //     }
+    //   },
+    //   component: () => import(/* webpackChunkName: "transfer-page" */ './views/transfer')
+    // }, {
+    //   path: '/login',
+    //   name: 'login',
+    //   meta: {
+    //     accent: 'accent'
+    //   },
+    //   component: () => import(/* webpackChunkName: "login-page" */ './views/login')
+    // }, {
+    //   path: '/Init',
+    //   name: 'Init',
+    //   meta: {
+    //     title: 'Initialize',
+    //     accent: 'accent'
+    //   },
+    //   component: () => import(/* webpackChunkName: "Init-page" */ './views/Init')
+    // }, {
+    //   path: '/error',
+    //   name: 'error',
+    //   meta: {
+    //     accent: 'accent'
+    //   },
+    //   component: () => import(/* webpackChunkName: "error-page" */ './views/errorpage')
+    // }, {
+    //   path: '*',
+    //   redirect: '404',
+    //   meta: {
+    //     accent: 'accent'
+    //   }
+    // }
 ];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes,
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes,
 });
 
 router.beforeEach((to, from, next) => {
-  if (!store.getters.initialized && to.name !== 'error' && to.name !== 'init') {
-    next({
-      name: 'init',
-    });
-    return;
-  }
-  next();
+    if (!store.getters.initialized && to.name !== 'error' && to.name !== 'init') {
+        next({
+            name: 'init',
+        });
+        return;
+    }
+    next();
 });
 
 export default router;
