f583042eec638b4311c6f6ecca1ee146c70ebbe0
Jonas Wijne <jonas.wijne@jimber.org>
2020-02-25 15:34:23 +0100

    update styling

--- src/utils/filters/index.js
+++ src/utils/filters/index.js
@@ -2,9 +2,9 @@ import { formatBalance, formatBalanceHumanReadable } from './formatBalance';
 import { formatDate } from './formatDate';
 
 export default {
-    install(Vue) {
-        Vue.filter('formatBalance', formatBalance);
-        Vue.filter('formatBalanceHumanReadable', formatBalanceHumanReadable);
-        Vue.filter('formatDate', formatDate);
-    },
+  install (Vue) {
+    Vue.filter('formatBalance', formatBalance);
+    Vue.filter('formatBalanceHumanReadable', formatBalanceHumanReadable);
+    Vue.filter('formatDate', formatDate);
+  },
 };
--- src/views/errorScreen/errorScreen.js
+++ src/views/errorScreen/errorScreen.js
@@ -1,24 +1,18 @@
 export default {
-    name: 'error-screen',
-    components: {},
-    props: [],
-    data() {
-        return {
-            reason: null,
-            fix: null,
-        }
-    },
-    computed: {
-
-    },
-    mounted() {
-
-    },
-    methods: {
-
-    },
-    beforeMount() {
-        this.reason = this.$route.params.reason;
-        this.fix = this.$route.params.fix;
-    },
-}
+  name: 'error-screen',
+  components: {},
+  props: [],
+  data () {
+    return {
+      reason: null,
+      fix: null,
+    };
+  },
+  computed: {},
+  mounted () {},
+  methods: {},
+  beforeMount () {
+    this.reason = this.$route.params.reason;
+    this.fix = this.$route.params.fix;
+  },
+};
