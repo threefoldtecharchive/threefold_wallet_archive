28f90bb9e8acb22c5b238a234d47c11813f1cf71
Tobias Chielens <tobias.chielens@gmail.com>
2020-02-27 15:08:55 +0100

    fixed 1m+ tokens transfer issue

--- src/views/transfer/components/formComponent/formComponent.js
+++ src/views/transfer/components/formComponent/formComponent.js
@@ -59,7 +59,7 @@ export default {
         v => !!v && Number(v) > 0 || 'The amount must be greater than 0'
       ]
       if (['send', 'deregister', 'register'].some(x => x === this.$route.query.tab)) {
-        rules.push(v => !!v && Number(v) <= Number((this.wallets.find(x => x.address == this.selectedWallet.address).totalAmount.replace(",", "") - 0.10).toFixed(9)) || 'Your balance is insufficient')
+        rules.push(v => !!v && Number(v) <= Number((this.wallets.find(x => x.address == this.selectedWallet.address).totalAmount.replace(/,/g, "") - 0.10).toFixed(9)) || 'Your balance is insufficient')
       }
       return rules
     },
