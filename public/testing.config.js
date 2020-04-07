5131b8cd591cfc12ee333b8cc157ffd6377b9b7d
Mathias <mathias@pop-os.localdomain>
2020-04-07 12:28:42 +0200

    Added testing ci/cd and config update

--- public/testing.config.js
+++ public/testing.config.js
@@ -1,5 +1,5 @@
 export default {
-    pkidUrl: 'https://pkid.staging.jimber.org',
+    pkidUrl: 'https://pkid.testing.jimber.org',
     stellarServerUrl: 'https://horizon-testnet.stellar.org',
     stellarNetwork: 'Test SDF Network ; September 2015',
     tftIssuer: 'GA47YZA3PKFUZMPLQ3B5F2E3CJIB57TGGU7SPCQT2WAEYKN766PWIMB3',
