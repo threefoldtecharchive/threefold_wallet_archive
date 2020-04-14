674d96c4abe53d8ca4216aa00ecd4ecc2c416806
Tobias Chielens <tobias.chielens@gmail.com>
2020-04-14 08:41:25 +0200

    fixed typo

--- src/views/Transfer/Transfer.js
+++ src/views/Transfer/Transfer.js
@@ -120,7 +120,7 @@ export default {
                 );
 
                 this.$flashMessage.info(
-                    `Successfully tranferred ${this.formObject.amount} to ${this.formObject.to.address}.`
+                    `Successfully transferred ${this.formObject.amount} to ${this.formObject.to.address}.`
                 );
             } catch {
                 //@todo show correct error message for multiple errors eg: "reason": "invalid address"
