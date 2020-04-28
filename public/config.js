ab39fab09b14eddb56a4d1d5b0b75d2a3e42cbdd
Tobias Chielens <tobias.chielens@gmail.com>
2020-04-28 10:35:05 +0200

    TFTA

--- public/config.js
+++ public/config.js
@@ -13,6 +13,10 @@ export default {
             asset_code: "TFT",
             issuer: "GA47YZA3PKFUZMPLQ3B5F2E3CJIB57TGGU7SPCQT2WAEYKN766PWIMB3",
         },
+        TFTA: {
+            asset_code: "TFTA",
+            issuer: "GB55A4RR4G2MIORJTQA4L6FENZU7K4W7ATGY6YOT2CW47M5SZYGYKSCT"
+        },
         FreeTFT: {
             asset_code: "FreeTFT",
             issuer: "GBLDUINEFYTF7XEE7YNWA3JQS4K2VD37YU7I2YAE7R5AHZDKQXSS2J6R",
