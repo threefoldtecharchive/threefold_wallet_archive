e1ecedd2287b369d85220d34f0a09891987f9519
Tobias Chielens <tobias.chielens@gmail.com>
2020-04-30 14:14:04 +0200

    Adding trustlines in dev view

--- src/views/DevView/components/AddTrustlineCard.vue
+++ src/views/DevView/components/AddTrustlineCard.vue
@@ -1,54 +1,48 @@
 <template>
     <v-card>
+        <v-card-title>
+            Add threefold trustlines 
+        </v-card-title>
         <v-card-text>
-            <p class="display-1 text--primary">
-                Add trustline
-            </p>
-            <v-select
-                :items="accounts"
-                item-text="name"
-                return-object
-                label="Account"
-                hint="select account to add trustlines to"
-                persistent-hint
-                v-model="account"
-            ></v-select>
-            <v-select
-                :items="Object.keys(currencies)"
-                label="asset_code"
-                multiple
-                hint="select trustlines to add"
-                persistent-hint
-                v-model="assetCodes"
-            ></v-select>
+
+            <v-col>
+                <v-row>
+                    Available trustlines: 
+                    <span class="ml-1" v-for="currency in currencies" :key="currency"> {{currency}} </span>
+                </v-row>
+                <v-row v-for="account in accounts" :key=account.id>
+                {{account.name}}
+                <v-spacer></v-spacer>
+                <v-btn x-small @click="addTrustLines(account)" >Add Threefold trustlines</v-btn>
+                </v-row>
+            </v-col>
         </v-card-text>
+        <!-- 
         <v-card-actions>
             <v-btn dark text color="deep-purple" @click="addTrustLines">
                 addtrustlines
             </v-btn>
-        </v-card-actions>
+        </v-card-actions> -->
     </v-card>
 </template>
 <script>
     import { mapGetters } from 'vuex';
     import config from '../../../../public/config';
     import { Server } from 'stellar-sdk';
+    import { addTrustLine } from '@jimber/stellar-crypto'
 
     export default {
         name: 'addTrustlineCard',
         computed: {
-            ...mapGetters(['accounts']),
-            currencies: config.currencies
+            ...mapGetters(['accounts', 'currencies']),
         },
         data: function () {
             return { account: null, assetCodes: null };
         },
         methods: {
-            assetCodeValue(...args) {
-                console.log(args);
-            },
-            addTrustLines() {
-                //@todo: implement this
+            addTrustLines(account) {
+                console.log(account)
+                addTrustLine(account.keyPair)
             }
         }
     };
