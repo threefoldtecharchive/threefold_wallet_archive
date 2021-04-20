aba2d7d53e2ed7d26176dc76b40cb1c24607851f
Jonas Wijne <jonas.wijne@jimber.org>
2021-04-20 17:40:46 +0200

    add rules

--- src/views/Buy.vue
+++ src/views/Buy.vue
@@ -1,98 +1,111 @@
 <template>
     <div class="Buy pa-2 fill-height">
         <v-card class="pa-2" style="width: 100%">
-            <v-card-title> Account: {{ account.name }}</v-card-title>
-            <v-card-text>
-                <v-text-field
-                    v-model.number.lazy="btcAmount"
-                    persistent-hint
-                    label="Amount BTC"
-                    type="number"
-                    step="0.001"
-                    max="150"
-                    min="0.000001"
-                ></v-text-field>
-                <div class="btns">
-                    <v-btn
-                        class="pa-1 mr-2"
-                        small
-                        elevation="0"
-                        color="#e0e0e0"
-                        @click="btcAmount = account.balances.find(b => b.asset_code === 'BTC').balance * 0.25"
-                        >25%
-                    </v-btn>
-                    <v-btn
-                        class="pa-1 mr-2"
-                        small
-                        elevation="0"
-                        color="#e0e0e0"
-                        @click="btcAmount = account.balances.find(b => b.asset_code === 'BTC').balance * 0.5"
-                        >50%
-                    </v-btn>
-                    <v-btn
-                        class="pa-1 mr-2"
-                        small
-                        elevation="0"
-                        color="#e0e0e0"
-                        @click="btcAmount = account.balances.find(b => b.asset_code === 'BTC').balance * 0.75"
-                        >75%
-                    </v-btn>
-                    <v-btn
-                        class="pa-1 mr-2"
-                        small
-                        elevation="0"
-                        color="#e0e0e0"
-                        @click="btcAmount = account.balances.find(b => b.asset_code === 'BTC').balance"
-                        >100%
-                    </v-btn>
-                </div>
-                <v-text-field
-                    v-model="priceUsd"
-                    disabled
-                    persistent-hint
-                    label="amount in usd"
-                    class="mt-2"
-                    type="number"
-                ></v-text-field>
-                <hr class="my-4" />
-                <pre>
+            <v-form ref="form" v-model="valid" lazy-validation>
+                <v-card-title> Account: {{ account.name }}</v-card-title>
+                <v-card-text>
+                    <v-text-field
+                        v-model.number.lazy="btcAmount"
+                        persistent-hint
+                        label="Amount BTC"
+                        type="number"
+                        step="0.001"
+                        max="150"
+                        min="0.000001"
+                        required
+                        :rules="[
+                            v => !!v || 'Btc amount is required',
+                            v =>
+                                v <= account.balances.find(b => b.asset_code === 'BTC').balance ||
+                                'Minimum price is 0.1',
+                        ]"
+                    ></v-text-field>
+                    <div class="btns">
+                        <v-btn
+                            class="pa-1 mr-2"
+                            small
+                            elevation="0"
+                            color="#e0e0e0"
+                            @click="btcAmount = account.balances.find(b => b.asset_code === 'BTC').balance * 0.25"
+                            >25%
+                        </v-btn>
+                        <v-btn
+                            class="pa-1 mr-2"
+                            small
+                            elevation="0"
+                            color="#e0e0e0"
+                            @click="btcAmount = account.balances.find(b => b.asset_code === 'BTC').balance * 0.5"
+                            >50%
+                        </v-btn>
+                        <v-btn
+                            class="pa-1 mr-2"
+                            small
+                            elevation="0"
+                            color="#e0e0e0"
+                            @click="btcAmount = account.balances.find(b => b.asset_code === 'BTC').balance * 0.75"
+                            >75%
+                        </v-btn>
+                        <v-btn
+                            class="pa-1 mr-2"
+                            small
+                            elevation="0"
+                            color="#e0e0e0"
+                            @click="btcAmount = account.balances.find(b => b.asset_code === 'BTC').balance"
+                            >100%
+                        </v-btn>
+                    </div>
+                    <v-text-field
+                        v-model="priceUsd"
+                        disabled
+                        persistent-hint
+                        label="amount in usd"
+                        class="mt-2"
+                        type="number"
+                    ></v-text-field>
+                    <hr class="my-4" />
+                    <pre>
                 {{ amountTft }}
                   (1 / (btcAmount * price in tft)).toFixed(8)
 
                   price = this.currentBtcPrice / usdPrice
                 {{ (1 / price).toFixed(8) }}
               </pre
-                >
-                <!--                <v-text-field-->
-                <!--                    :value="priceInTFT"-->
-                <!--                    @change="onChangeTFTPrice"-->
-                <!--                    persistent-hint-->
-                <!--                    color="black"-->
-                <!--                    label="Price in TFT"-->
-                <!--                    hint="minimum a mount of of tft you will get"-->
-                <!--                    type="number"-->
-                <!--                ></v-text-field>-->
-                <v-text-field
-                    :value="priceInUSD"
-                    @change="onChangeUSDPrice"
-                    persistent-hint
-                    color="black"
-                    label="Price in Usd"
-                    hint="The price you want (in USD) for 1 TFT"
-                    type="number"
-                    step="0.01"
-                    min="0.1"
-                    max="150"
-                ></v-text-field>
-                <hr class="my-4" />
+                    >
+                    <!--                <v-text-field-->
+                    <!--                    :value="priceInTFT"-->
+                    <!--                    @change="onChangeTFTPrice"-->
+                    <!--                    persistent-hint-->
+                    <!--                    color="black"-->
+                    <!--                    label="Price in TFT"-->
+                    <!--                    hint="minimum a mount of of tft you will get"-->
+                    <!--                    type="number"-->
+                    <!--                ></v-text-field>-->
+                    <v-text-field
+                        :value="priceInUSD"
+                        @change="onChangeUSDPrice"
+                        persistent-hint
+                        color="black"
+                        label="Price in Usd"
+                        hint="The price you want (in USD) for 1 TFT"
+                        type="number"
+                        step="0.01"
+                        min="0.1"
+                        max="150"
+                        required
+                        :rules="[v => !!v || 'price is required', v => v >= 0.1 || 'Minimum price is 0.1']"
+                    ></v-text-field>
+                    <hr class="my-4" />
 
-                <p>
-                    lorem ipsuim dolor sit amed <b>{{ amountTft }}</b> TFT
-                </p>
-            </v-card-text>
-            <v-card-actions>
-                <v-btn color="accent" block elevation="0" @click="onSubmit">Submit this buy order</v-btn>
-            </v-card-actions>
+                    <p>
+                        lorem ipsuim dolor sit amed <b>{{ amountTft }}</b> TFT
+                    </p>
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn color="accent" block elevation="0" @click="onSubmit" :disabled="!valid"
+                        >Submit this buy order</v-btn
+                    >
+                </v-card-actions>
+            </v-form>
         </v-card>
     </div>
 </template>
@@ -134,6 +147,7 @@
                 currentBtcPrice: 0,
                 price: 0,
                 usdPrice: 0.15,
+                valid: null,
             };
         },
         mounted() {
